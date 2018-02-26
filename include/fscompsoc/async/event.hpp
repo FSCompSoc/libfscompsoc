78f862c2cf1a1d74fe629d1f8c877b79853acaaa
Cyclic3 <cyclic3.git@gmail.com>
2018-02-26 11:49:50 +0000

    Completed events
    Done some more formatting

--- include/fscompsoc/async/event.hpp
+++ include/fscompsoc/async/event.hpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <list>
 #include <mutex>
+#include <thread>
 
 namespace fscompsoc::async
 {
@@ -19,41 +20,59 @@ namespace fscompsoc::async
 
   private:
     std::function<void(T)> handler;
-    typename std::list<T>::iterator pos;
+    typename std::list<std::shared_ptr<event_handler<T>>>::iterator pos;
+    event_token<T>* parent;
 
   public:
-    void disconnect();
+    inline void operator()(T t) { handler(t); }
+
+  public:
+    event_handler(event_handler&) = delete;
+
+    ~event_handler();
   };
 
   template <typename T>
   class event_token
   {
     friend event<T>;
+    friend event_handler<T>;
 
   private:
-    std::list<event_handler<T>> handlers;
+    std::list<std::shared_ptr<event_handler<T>>> handlers;
     std::mutex handlers_mutex;
+    typename std::list<std::shared_ptr<event_token<T>>>::iterator pos;
+    event<T>* parent;
 
   public:
-    event_handler<T> add_handler(std::function<T()> handler);
+    std::shared_ptr<event_handler<T>> add_handler(std::function<T()> handler);
 
   private:
-    void trigger(T);
+    std::thread trigger(T);
 
   private:
     event_token() = default;
+
+  public:
+    event_token(event_token&);
+
+    ~event_token();
   };
 
   template <typename T>
   class event
   {
+    friend event_token<T>;
+
   private:
-    std::list<event_token<T>> tokens;
+    std::list<std::shared_ptr<event_token<T>>> tokens;
     std::mutex tokens_mutex;
 
   public:
-    event_token<T> create_token();
+    std::shared_ptr<event_token<T>> create_token();
 
-    inline void trigger(T);
+    void trigger(T);
+
+    inline void operator()(T t) { trigger(t); }
   };
 } // namespace fscompsoc::async
--- src/async/event.cpp
+++ src/async/event.cpp
@@ -1,16 +1,91 @@
 #include "fscompsoc/async/event.hpp"
 
+#include <vector>
+
+using namespace std;
+
 namespace fscompsoc::async
 {
   template <typename T>
-  event_token<T> event<T>::create_token()
+  shared_ptr<event_token<T>> event<T>::create_token()
   {
-    event_token<T> ret;
+    shared_ptr<event_token<T>> ret(new event_handler<T>);
+
+    ret->parent = this;
 
     tokens_mutex.lock();
-    ret.push_front();
+    tokens.push_front(ret);
+    ret->pos = tokens.front;
     tokens_mutex.unlock();
 
     return ret;
   }
+
+  template <typename T>
+  shared_ptr<event_handler<T>>
+  event_token<T>::add_handler(std::function<T()> handler)
+  {
+    shared_ptr<event_token<T>> ret(new event_token<T>);
+
+    ret->handler = handler;
+    ret->parent  = this;
+
+    handlers_mutex.lock();
+    handlers.push_front(ret);
+    ret->pos = handlers.front;
+    handlers_mutex.unlock();
+
+    return ret;
+  }
+
+  template <typename T>
+  event_token<T>::~event_token()
+  {
+    // We need to lock to make sure that we are not accessed whilst being
+    // deleted
+    parent->tokens_mutex.lock();
+    parent->tokens.erase(pos);
+    parent->tokens_mutex.unlock();
+  }
+
+  template <typename T>
+  event_handler<T>::~event_handler()
+  {
+    // We need to lock to make sure that we are not accessed whilst being
+    // deleted
+    parent->handlers_mutex.lock();
+    parent->handlers.erase(pos);
+    parent->handlers_mutex.unlock();
+  }
+
+  template <typename T>
+  void event<T>::trigger(T t)
+  {
+    vector<thread> threads;
+
+    tokens_mutex.lock();
+    threads.reserve(tokens.size());
+
+    for (auto& token : tokens)
+      threads.push_back(token.trigger(t));
+
+    for (auto& thread : threads)
+      thread.join();
+
+    tokens_mutex.unlock();
+  }
+
+  template <typename T>
+  thread event_token<T>::trigger(T t)
+  {
+    return thread([this, t] {
+      handlers_mutex.lock();
+
+      for (auto& handler : handlers)
+        handler(t);
+
+      handlers_mutex.unlock();
+    });
+  }
+
 } // namespace fscompsoc::async
